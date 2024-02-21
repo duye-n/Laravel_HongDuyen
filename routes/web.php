613b2297788f74fe6c820c63ac11619ee17df4c2
duyen <duyen.dinh25@student.passerellesnumeriques.org>
2024-02-21 21:52:26 +0700

    updat

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,4 @@
-<?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
